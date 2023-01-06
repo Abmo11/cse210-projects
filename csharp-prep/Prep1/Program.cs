07414b927db9cb1236bdbc069aa9533c46074c94
Abmo11 <mor18053@byui.edu>
2023-01-06 13:44:18 -0600

    Finished prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Asks for first name
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        // Asks for last name
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        // Adds a space in between the inputs and final output
+        Console.WriteLine();
+
+        // Displays name 007 style
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
