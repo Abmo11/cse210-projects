680c45977f4b078384b94280ae0e677af0f551ca
Abmo11 <mor18053@byui.edu>
2023-01-07 10:00:42 -0600

    Core Requirements 1 & 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // Asks user for grade percentage, stores input
+        Console.Write("Please enter your grade percentage: ");
+        string gradePercent = Console.ReadLine();
+
+        int grade = int.Parse(gradePercent);
+
+        // Matches input to letter grade
+        if (grade >= 90)
+        {
+            Console.WriteLine("A");
+        }
+        else if (grade >= 80)
+        {
+            Console.WriteLine("B");
+        }
+        else if (grade >= 70)
+        {
+            Console.WriteLine("C");
+        }
+        else if (grade >= 60)
+        {
+            Console.WriteLine("D");
+        }
+        else
+        {
+            Console.WriteLine("F");
+        }
+
+        // Determines if student passed the class
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you didn't pass. Try again and you will do better.");
+        }
     }
 }
