f2a06a01d1a0f7e55db7aeec2ddb8977473750d9
Abmo11 <mor18053@byui.edu>
2023-01-10 16:26:35 -0600

    Stretch Challenge

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,36 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Restructured code to include a single do-while loop
-        // to consolidate both if blocks. Also found cleaner way
-        // to parse into ints from user input.
-
-
         // Creates a random number
         Random randomGenerator = new Random();
         int magic = randomGenerator.Next(1, 101);
 
-        // Variable for input
-        int magicGuess;
+        // Variables for input and guess counter
+        int magicGuess = 0;
+        int counter = 0;
 
         do
         {
             // Input parsed into an int
             Console.Write("Please enter your guess: ");
             magicGuess = int.Parse(Console.ReadLine());
+            counter++;
 
             if (magicGuess > magic)
             {
                 Console.WriteLine("Lower");
             }
-            else if (magicGuess == magic)
-            {
-                Console.WriteLine("You guessed it!");
-            }
-            else
+            else if (magicGuess < magic)
             {
                 Console.WriteLine("Higher");
-            } 
-        } while (magicGuess != magic); // Continues running until the number is guessed
+            }
+            else
+            {   // Gives option of playing again when you win
+                Console.WriteLine($"You guessed it! You made {counter} guesses.");
+                Console.WriteLine("Would you like to play again? (Yes or No)");
+                string answer = Console.ReadLine();
+
+                if (answer == "Yes")
+                {
+                    magic = randomGenerator.Next(1, 101);
+                    magicGuess = 0;
+                    counter = 0;
+                    Console.WriteLine("Let's play again!");
+                }
+                else
+                {
+                    Console.WriteLine("Thank you for playing.");
+                }
+            }
+        } while (magicGuess != magic) ; // Continues running until the number is guessed
     }
 }
