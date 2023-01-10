0af009dd7bdf2ff054714a5090e32eace1c25f7c
Abmo11 <mor18053@byui.edu>
2023-01-10 14:16:02 -0600

    Reqs 1 & 2

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("Please enter the magic number: ");
+        string input = Console.ReadLine();
+        int magic = int.Parse(input);
+
+        Console.Write("Please enter your guess: ");
+        string inputTwo = Console.ReadLine();
+        int magicGuess = int.Parse(inputTwo);
+
+
+        if (magicGuess > magic)
+        {
+            Console.WriteLine("Lower");
+        }
+        else if (magicGuess == magic)
+        {
+            Console.WriteLine("You guessed it!");
+        }
+        else
+        {
+            Console.WriteLine("Higher");
+        }
+
+        while (magicGuess != magic)
+        {
+            Console.Write("Please enter another guess: ");
+            string inputThree = Console.ReadLine();
+            magicGuess = int.Parse(inputThree);
+
+            if (magicGuess > magic)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (magicGuess == magic)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+            else
+            {
+                Console.WriteLine("Higher");
+            }
+        }
     }
 }
