b2e6217a32c1b2dd098cb613e99f92872ec55546
Abmo11 <mor18053@byui.edu>
2023-01-11 11:23:21 -0600

    Req 3

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using System.Collections.Generic;
+
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a series of numbers, type 0 when finished: ");
+
+        List<int> series = new List<int>();
+
+        int numbers = 0;
+        do
+        {
+            Console.Write("Enter number: ");
+            numbers = int.Parse(Console.ReadLine());
+
+            if (numbers != 0)
+            {
+                series.Add(numbers);
+            }
+        } while (numbers != 0);
+
+        int sum = 0;
+        double average = 0;
+        foreach (int numeros in series)
+        {
+            sum += numeros;
+            average = ((double)sum) / series.Count;
+        }
+        
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+
+        int max = 0;
+        foreach (int num in series)
+        {
+            if (num > max)
+            {
+                max = num;
+            }
+        }
+        
+        Console.WriteLine($"The largest number is: {max}");
     }
 }
