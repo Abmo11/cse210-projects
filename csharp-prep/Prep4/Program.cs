d51da9ad6d3d843fb0588f857e49cfc1b709c5d7
Abmo11 <mor18053@byui.edu>
2023-01-11 11:39:11 -0600

    Details, cleaned

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -8,6 +8,7 @@ class Program
     {
         Console.WriteLine("Enter a series of numbers, type 0 when finished: ");
 
+        // Creates empty list
         List<int> series = new List<int>();
 
         int numbers = 0;
@@ -16,23 +17,30 @@ class Program
             Console.Write("Enter number: ");
             numbers = int.Parse(Console.ReadLine());
 
-            if (numbers != 0)
+            // Adds number to the list as long as it's not 0
+            if (numbers != 0) 
             {
                 series.Add(numbers);
             }
-        } while (numbers != 0);
+        } while (numbers != 0); // Runs as long as 0 is not entered
 
+        // Adds all numbers in list
         int sum = 0;
-        double average = 0;
         foreach (int numeros in series)
         {
             sum += numeros;
-            average = ((double)sum) / series.Count;
         }
+
+        // Calculates average into double.
+        // If you don't cast sum into double it will do
+        // int division and cut off decimal places
+        double average = ((double)sum) / series.Count;
         
         Console.WriteLine($"The sum is: {sum}");
         Console.WriteLine($"The average is: {average}");
 
+        // Compares numbers in list to 0.
+        // Changes max to greatest # as it goes through list
         int max = 0;
         foreach (int num in series)
         {
